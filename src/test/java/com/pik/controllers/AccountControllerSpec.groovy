ba6e0a6ac853ac4a305986a7d32f007342899e85
HubertLegec <hubert.legec@gmail.com>
2016-04-04 23:26:42 +0200

    simlpe spock test

---
+++ src/test/java/com/pik/controllers/AccountControllerSpec.groovy
@@ -0,0 +1,8 @@
+package com.pik.controllers
+
+import spock.lang.Specification
+
+
+class AccountControllerSpec extends Specification {
+    //TODO
+}
--- src/test/java/com/pik/utils/CustomPasswordValidatorSpec.groovy
+++ src/test/java/com/pik/utils/CustomPasswordValidatorSpec.groovy
@@ -1,13 +1,18 @@
 package com.pik.utils
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 class CustomPasswordValidatorSpec extends Specification {
+    @Shared CustomPaswordValidator validator
 
-    def "validation of empty password string should fail"() {
+    def setup(){
+        validator = new CustomPaswordValidator()
+    }
+
+    def 'validation of empty password string should fail'() {
         given:
-            CustomPaswordValidator validator = new CustomPaswordValidator()
-            String password = ""
+            String password = ''
         when:
             boolean result = validator.validatePassword(password)
         then:
@@ -16,13 +21,36 @@ class CustomPasswordValidatorSpec extends Specification {
             validator.getLastMessage().size() > 0
     }
 
-    def "validation of proper password should return true"() {
+    def 'validation of password with number sequence should return false'() {
         given:
-            CustomPaswordValidator validator = new CustomPaswordValidator()
-            String password = "!Abcdef123"
+            String password = '!Abcdef123'
         when:
             boolean result = validator.validatePassword(password)
         then:
             result == false
+            validator.getLastMessage() != null
+            validator.getLastMessage().size() > 0
+    }
+
+    def 'validation of password with qwerty sequence should return false'(){
+        given:
+            String password = 'qwerty!2Avd'
+        when:
+            boolean result = validator.validatePassword(password)
+        then:
+            result == false
+            validator.getLastMessage() != null
+            validator.getLastMessage().size() > 0
+    }
+
+    def 'validation of valid password should return true'(){
+        given:
+        String password = 'A1b4ls!^sad'
+        when:
+        boolean result = validator.validatePassword(password)
+        then:
+        result == true
+        validator.getLastMessage() != null
+        validator.getLastMessage().size() == 0
     }
 }
